f3971ca051714a1e0d60f7080f3634ded4969f76
Shohag3363 <125680110+Shohag3363@users.noreply.github.com>
2024-04-11 16:30:36 +0600

    Add files via upload

---
+++ Mad-libs-game.py
@@ -0,0 +1,8 @@
+user=input("What is your name:")
+math=input("10*10:")
+math1=input("5-7+4-2:")
+
+print(f"Hello Mr./Mrs. {user}. "
+      f"\nYour attitude level is {math}/100."
+      f"\nBut, your performance level is {math1}/100."
+      f"\nEkhane ki? Soja porte bosh okormaa!!")
